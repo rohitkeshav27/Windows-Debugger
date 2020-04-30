51ffaf8c100f41f4c8726a58e2d1a205cb7efb7e
Rohit Keshav <43274184+rohitkeshav27@users.noreply.github.com>
2020-04-30 12:09:14 +0530

    Add files via upload

---
+++ mytest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 20:06:00 2020
+
+@author: rohit
+"""
+
+
+import my_debugger
+debugger = my_debugger.debugger()
+
+debugger.load("C:\\Windows\\System32\\calc.exe")
